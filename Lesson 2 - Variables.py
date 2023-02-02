36afa3c9a0b78ee9b94c62fc283432e511ce5f1f
Teacher Joseph <joseph.opio@bisp.edu.my>
2023-02-02 09:47:22 +0800

    Lesson 3 file

---
+++ Lesson 2 - Variables.py
@@ -0,0 +1,33 @@
+age = 16
+name = "Joseph"
+milk = 2.3
+children = ["Ben", "Faith", "Wisdom"]
+
+new_milk = int(milk)
+new_age = float(age)
+
+print(type(new_age))
+print(type(name))
+print(type(new_milk))
+print(type(children))
+
+# Arithmetic
+
+a = 7
+b= 8
+
+# Calculate the sum, difference, product and quotient
+
+# Simple string operations
+c = "loves"
+d = "likes"
+e = "Justine"
+f = "Tom"
+g = "hates"
+h = "Peter"
+i = "food"
+
+# What is the output of:
+#   f + c + e
+#   h + g + i
+#   i * 4
